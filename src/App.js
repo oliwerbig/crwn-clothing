c5386d85f44a86eee839f309394143e2eed12b0d
oliwerbig <oliver.emanuel.nagy@gmail.com>
2020-12-06 13:27:41 +0100

    User Redirect and User Action Type

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Route, Switch } from 'react-router-dom'
+import { Redirect, Route, Switch } from 'react-router-dom'
 import './App.css'
 import HomePage from './pages/homepage/homepage.component'
 import { ShopPage } from './pages/shop/shop.component'
@@ -41,15 +41,24 @@ class App extends Component {
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
-          <Route path='/signin' component={SignInAndSignUpPage} />
+          <Route exact path='/signin' render={() => this.props.currentUser ? (
+            <Redirect to='/' />
+          ) : (
+              <SignInAndSignUpPage />
+            )}
+          />
         </Switch>
       </div >
     )
   }
 }
 
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser
+})
+
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 })
 
-export default connect(null, mapDispatchToProps)(App)
+export default connect(mapStateToProps, mapDispatchToProps)(App)
