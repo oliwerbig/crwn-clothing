762e7c7a6e183e5917562598cebd723a3926c242
oliwerbig <oliver.emanuel.nagy@gmail.com>
2020-12-12 10:50:17 +0100

    Checkout Item Component

--- src/App.js
+++ src/App.js
@@ -8,6 +8,9 @@ import SignInAndSignUpPage from './components/sign-in-and-sign-up/sign-in-and-si
 import { auth, createUserProfileDocument } from './firebase/firebase.utils'
 import { connect } from 'react-redux'
 import { setCurrentUser } from './redux/user/user.actions'
+import { createStructuredSelector } from 'reselect'
+import { selectCurrentUser } from './redux/user/user.selector'
+import CheckoutPage from './pages/checkout/checkout.component'
 
 class App extends Component {
   unsubscribeFromAuth = null
@@ -41,6 +44,7 @@ class App extends Component {
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
+          <Route exact path='/checkout' component={CheckoutPage} />
           <Route exact path='/signin' render={() => this.props.currentUser ? (
             <Redirect to='/' />
           ) : (
@@ -53,8 +57,8 @@ class App extends Component {
   }
 }
 
-const mapStateToProps = ({ user }) => ({
-  currentUser: user.currentUser
+const mapStateToProps = createStructuredSelector({
+  currentUser: selectCurrentUser
 })
 
 const mapDispatchToProps = dispatch => ({
