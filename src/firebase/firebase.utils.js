82a203f2bf336c3d88cd558f36e7172bfd03bfad
oliwerbig <oliver.emanuel.nagy@gmail.com>
2020-12-14 14:09:59 +0100

    Promise Pattern

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -15,7 +15,7 @@ const config = {
 firebase.initializeApp(config)
 
 export const createUserProfileDocument = async (userAuth, ...additionalData) => {
-    if(!userAuth) return
+    if (!userAuth) return
 
     const userRef = firestore.doc(`users/${userAuth.uid}`)
 
@@ -38,6 +38,38 @@ export const createUserProfileDocument = async (userAuth, ...additionalData) => 
     }
     return userRef
 }
+
+export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => {
+    const collectionRef = firestore.collection(collectionKey)
+
+    const batch = firestore.batch()
+    objectsToAdd.forEach(obj => {
+        const newDocRef = collectionRef.doc()
+        batch.set(newDocRef, obj)
+
+    })
+
+    return await batch.commit()
+}
+
+export const convertCollectionsSnapshotToMap = (collections) => {
+    const transformedCollection = collections.docs.map(doc => {
+        const { title, items } = doc.data()
+
+        return {
+            routeName: encodeURI(title.toLowerCase()),
+            id: doc.id,
+            title,
+            items
+        }
+    })
+
+    transformedCollection.reduce((accumulator, collection) => {
+        accumulator[collection.title.toLowerCase()] = collection
+        return accumulator
+    }, {})
+}
+
 export const auth = firebase.auth()
 export const firestore = firebase.firestore()
 
