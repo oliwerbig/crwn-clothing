86d9bbe1ad644dae39c2b5ee876450062a6af9ee
oliwerbig <oliver.emanuel.nagy@gmail.com>
2021-01-09 14:08:28 +0100

    Fixed serviceWorker registration

--- client/src/index.js
+++ client/src/index.js
@@ -5,7 +5,7 @@ import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 
 import { store, persistor } from './redux/store';
-import * as serviceWorker from "./service-worker"
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 import './index.css';
 import App from './App';
@@ -22,5 +22,5 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-ServiceWorkerRegistration.register()
+serviceWorkerRegistration.register()
 reportWebVitals()
